b59041f9453811cfea93a9100c4737f46ab0345e
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-03-29 09:20:08 +0800

    20230329

--- src/components/Polygon.js
+++ src/components/Polygon.js
@@ -3,59 +3,68 @@ import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Polygon } from "react-leaflet";
 
 export default function CurPolygon() {
-    const [data, setData] = useState();
-    // const curLocation = { lat: 40.848447, long: -73.856077 };
-    let currentPolygon = [];
-    const [polyArr, setPolyArr] = useState([]);
-    const purpleOptions = { color: "purple" };
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  // const curLocation = { lat: 40.848447, long: -73.856077 };
+  let currentPolygon = [];
+  const [polyArr, setPolyArr] = useState([]);
+  const purpleOptions = { color: "purple" };
 
-    useEffect(() => {
-        getData();
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    function getData() {
-        axios
-            .get("http://localhost:9000/api/neighborhoods")
-            .then((res) => {
-                console.log(res.data.result);
-                setData(res.data.result);
-                changePosition(res.data.result);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
+  useEffect(() => {
+    changePosition(data);
+  }, [loading]);
 
-    function changePosition(arr) {
-        const newAr = [];
+  function getData() {
+    axios
+      .get("http://localhost:9000/api/neighborhoods")
+      .then((res) => {
+        console.log(res.data.result);
+        setData(res.data.result);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
 
-        arr?.map((item1) => {
-            item1?.geometry.coordinates[0].map((item2) => {
-                currentPolygon.push([item2[1], item2[0]]);
-            });
-            console.log(currentPolygon);
-            newAr.push(currentPolygon);
+  function changePosition(arr) {
+    const newAr = [];
 
-            //  setPolyArr([...polyArr, currentPolygon]);
-            currentPolygon = [];
-        });
-        setPolyArr(newAr);
-    }
-    console.log(polyArr);
+    arr?.map((item1) => {
+      item1?.geometry?.coordinates[0].map((item2) => {
+        currentPolygon.push([item2[1], item2[0]]);
+      });
+      console.log(currentPolygon);
+      newAr.push(currentPolygon);
+      currentPolygon = [];
+    });
+    setPolyArr(newAr);
+  }
+  console.log(polyArr);
 
+  if (loading) {
+    return <h1>Loading</h1>;
+  } else {
     return (
-        <MapContainer
-            center={[40.848447, -73.856077]}
-            zoom={14}
-            scrollWheelZoom={false}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {polyArr?.map((item) => {
-                return <Polygon pathOptions={purpleOptions} positions={item} />;
-            })}
-        </MapContainer>
+      <MapContainer
+        center={[40.848447, -73.856077]}
+        zoom={14}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {polyArr?.map((item) => {
+          return <Polygon pathOptions={purpleOptions} positions={item} />;
+        })}
+      </MapContainer>
     );
+  }
 }
